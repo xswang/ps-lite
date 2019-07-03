2949b130efee985b10b3fb7c26b33574c945e8d7
xswang <2012wxs@gmail.com>
2019-07-03 09:08:49 +0800

    update: cmake/ProtoBuf.cmake for buildin pdl

--- cmake/ProtoBuf.cmake
+++ cmake/ProtoBuf.cmake
@@ -1,17 +1,17 @@
 # Finds Google Protocol Buffers library and compilers and extends
 # the standard cmake script with version and python generation support
 
-find_package( Protobuf REQUIRED )
+#find_package( Protobuf REQUIRED )
 include_directories(SYSTEM ${PROTOBUF_INCLUDE_DIR})
 
 
 # As of Ubuntu 14.04 protoc is no longer a part of libprotobuf-dev package
 # and should be installed separately as in: sudo apt-get install protobuf-compiler
-if(EXISTS ${PROTOBUF_PROTOC_EXECUTABLE})
-  message(STATUS "Found PROTOBUF Compiler: ${PROTOBUF_PROTOC_EXECUTABLE}")
-else()
-  message(FATAL_ERROR "Could not find PROTOBUF Compiler")
-endif()
+#if(EXISTS ${PROTOBUF_PROTOC_EXECUTABLE})
+#  message(STATUS "Found PROTOBUF Compiler: ${PROTOBUF_PROTOC_EXECUTABLE}")
+#else()
+#  message(FATAL_ERROR "Could not find PROTOBUF Compiler")
+#endif()
 
 
 # place where to generate protobuf sources
